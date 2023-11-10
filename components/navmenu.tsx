5d407bf1e8d76b10efbe966e1142113aca540904
Martin Diaz <mdiaz@handheldsolutions.com.mx>
2023-11-10 15:55:57 -0600

    Se agregaron extenciones a las demas paginas

--- components/navmenu.tsx
+++ components/navmenu.tsx
@@ -12,23 +12,28 @@ export default function navmenu() {
           <Nav className="me-auto">
             <Nav.Link href="#home">Inicio</Nav.Link>
             <NavDropdown title="Renta" id="renta">
-              <NavDropdown.Item href="#action/3.1">HandHeld</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Lector Códigos</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Impresoras </NavDropdown.Item>
+              <NavDropdown.Item href="/renta">HandHeld</NavDropdown.Item>
+              <NavDropdown.Item href="/renta">Lector Códigos</NavDropdown.Item>
+              <NavDropdown.Item href="/renta">Impresoras </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Venta" id="venta">
-              <NavDropdown.Item href="#action/3.1">HandHeld</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Lector Códigos</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Impresoras </NavDropdown.Item>
+              <NavDropdown.Item href="/venta">HandHeld</NavDropdown.Item>
+              <NavDropdown.Item href="/venta">Lector Códigos</NavDropdown.Item>
+              <NavDropdown.Item href="/venta">Impresoras </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Reparacion" id="reparacion">
-              <NavDropdown.Item href="#action/3.1">HandHeld</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Lector Códigos</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Impresoras </NavDropdown.Item>
+              <NavDropdown.Item href="/reparacion">HandHeld</NavDropdown.Item>
+              <NavDropdown.Item href="/reparacion">Lector Códigos</NavDropdown.Item>
+              <NavDropdown.Item href="/reparacion">Impresoras </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Servicios" id="servicios">
+              <NavDropdown.Item href="/credenciales">Impresion Credenciales</NavDropdown.Item>
+              <NavDropdown.Item href="/etiquetas">Impresion Etiquetas</NavDropdown.Item>
+              <NavDropdown.Item href="/dsd">Software DSD </NavDropdown.Item>
             </NavDropdown>
           
             <Nav.Link href="#home">Equipos</Nav.Link>
-            <Nav.Link href="#home">Nosotros</Nav.Link>
+            <Nav.Link href="/nosotros">Nosotros</Nav.Link>
             <NavDropdown title="Contenido" id="contenido">
               <NavDropdown.Item href="#action/3.1">Videos</NavDropdown.Item>
             </NavDropdown>
